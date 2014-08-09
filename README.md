d280b2c776e1d4bfd27c84edd00abefadb7f378d
deseven <de7@deseven.info>
2014-08-09 16:30:18 +0600

    added dependencies test

--- README.md
+++ README.md
@@ -4,12 +4,15 @@ a php class for bpm detection
 ## dependencies
 php 5.2 or higher  
 [soundtouch](http://www.surina.net/soundtouch/)  
-sox and mpg123 **OR** ffmpeg
+sox  
+mpg123  
+ffmpeg (optional, can be used instead of sox and mpg123)  
 
 ## compatibility
 tested on php 5.3.3, sox 14.2.0, mpg123 1.20.1, ffmpeg 0.6.5, soundtouch 1.8
 
 ## basic usage
+important: **run the `test-dep.php` to test the dependencies**
 ```php
 $bpm_detect = new bpm_detect("file.mp3");  
 echo $bpm_detect->detectBPM();
---
+++ test-dep.php
@@ -0,0 +1,90 @@
+<?php
+
+$isCLI = (php_sapi_name() == 'cli');
+
+function getSoxVersion() {
+	exec("sox --version 2>&1",$sox_version);
+	foreach ($sox_version as $line) {
+		if (strpos($line,"SoX") !== false) {
+			preg_match("/\d+\.\d+\.\d+/",$line,$match);
+			return $match[0];
+		}
+	}
+	return false;
+}
+
+function getMpg123Version() {
+	exec("mpg123 --version 2>&1",$mpg123_version);
+	foreach ($mpg123_version as $line) {
+		if (strpos($line,"mpg123") !== false) {
+			preg_match("/\d+\.\d+\.\d+/",$line,$match);
+			return $match[0];
+		}
+	}
+	return false;
+}
+
+function getSoundStretchVersion() {
+	exec("soundstretch 2>&1",$soundstretch_version);
+	foreach ($soundstretch_version as $line) {
+		if (strpos($line,"SoundStretch") !== false) {
+			preg_match("/\d+\.\d+\.\d+/",$line,$match);
+			return $match[0];
+		}
+	}
+	return false;	
+}
+
+function getFFmpegVersion() {
+	exec("ffmpeg -version 2>&1",$ffmpeg_version);
+	foreach ($ffmpeg_version as $line) {
+		if (strpos($line,"FFmpeg version") !== false) {
+			preg_match("/\d+\.\d+\.\d+/",$line,$match);
+			return $match[0];
+		}
+	}
+	return false;	
+}
+
+$soundstretch_version = getSoundStretchVersion();
+$sox_version = getSoxVersion();
+$mpg123_version = getMpg123Version();
+$ffmpeg_version = getFFmpegVersion();
+
+echo "SoundTouch: \t";
+if ($soundstretch_version !== false) {
+	echo "[PASS] detected SoundStretch ".$soundstretch_version;
+} else {
+	echo "[FAIL] can't find a suitable SoundStretch";
+}
+
+echo ($isCLI ? "\n" : "<br>");
+
+echo "SoX:        \t";
+if ($sox_version !== false) {
+	echo "[PASS] detected SoX ".$sox_version;
+} else {
+	echo "[FAIL] can't find a suitable SoX";
+}
+
+echo ($isCLI ? "\n" : "<br>");
+
+echo "mpg123:     \t";
+if ($mpg123_version !== false) {
+	echo "[PASS] detected mpg123 ".$mpg123_version;
+} else {
+	echo "[FAIL] can't find a suitable mpg123";
+}
+
+echo ($isCLI ? "\n" : "<br>");
+
+echo "ffmpeg:     \t";
+if ($ffmpeg_version !== false) {
+	echo "[PASS] detected ffmpeg ".$ffmpeg_version;
+} else {
+	echo "[FAIL] can't find a suitable ffmpeg";
+}
+
+echo ($isCLI ? "\n" : "<br>");
+
+?>
